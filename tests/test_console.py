33e452fe84616ddd64068db4158af40bbaaaf0bc
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-09-17 16:53:37 +0000

    fix console 4

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
-""" """
-from models.base_model import BaseModel
+"""A unit test to test the console"""
 import unittest
-import datetime
-import json
 import os
-from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+from tests import clear_stream
 from models import storage
-from models.base_model import BaseModel
-from models.user import User
+from console import HBNBCommand
 
-class Testconsole(unittest.TestCase):
-    """ """
-    pass
+
+class TestConsole(unittest.TestCase):
+    """test class for the HBNBCommand class."""
+    @unittest.skipIf(
+            os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
+    def test_create(self):
+        """test create in console"""
+        with patch('sys.stdout', new=StringIO()) as print_out:
+            console = HBNBCommand()
+            console.onecmd('create City name="Texas"')
+            mid = print_out.getvalue().strip()
+            clear_stream(print_out)
+            self.assertIn('City.{}'.format(mid), storage.all().keys())
+            console.onecmd('show City {}'.format(mid))
+            self.assertIn("'name': 'Texas'", print_out.getvalue().strip())
+            clear_stream(print_out)
+            console.oncmd('create User name="James" age=17 height=5.9')
+            mid = print_out.getvalue().strip()
+            self.assertIn('User.{}'.format(mid), storage.all().keys())
+            clear_stream(print_out)
+            console.onecmd('show User {}'.format(mid))
+            self.assertIn("'name': 'James'", print_out.getvalue().strip())
+            self.assertIn("'age': 17", print_out.getvalue().strip())
+            self.assertIn("'height': 5.9", print_out.getvalue().strip())
