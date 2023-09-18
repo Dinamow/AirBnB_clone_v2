fe5d648eeb35295e897e8ff81b2d7740dddfdcae
DINAMOW <meemoo102039@gmail.com>
2023-09-18 22:47:33 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -37,8 +37,6 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        if self.__dict__['_sa_instance_state']:
-            del (self.__dict__['_sa_instance_state'])
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""this is class file"""
+
+
+class db_storage:
+    """this is class"""
+    __engine = None
