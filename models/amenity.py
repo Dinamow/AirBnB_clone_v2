ee833b5388e34440b10b71d5bf73972114592762
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-09-25 11:35:59 +0000

    Test

--- models/amenity.py
+++ models/amenity.py
@@ -9,6 +9,9 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """Amenity class"""
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates="amenities")
+    else:
         name = ''
