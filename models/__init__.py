5a85f9319a279d22bb9fbcb59fe45cb28d10670e
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-09-25 09:41:40 +0000

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -4,11 +4,6 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import db_storage
 import os
 
-os.environ['HBNB_MYSQL_USER'] = 'hbnb_dev'
-os.environ['HBNB_MYSQL_PWD'] = 'hbnb_dev_pwd'
-os.environ['HBNB_MYSQL_HOST'] = 'localhost'
-os.environ['HBNB_TYPE_STORAGE'] = 'db'
-os.environ['HBNB_MYSQL_DB'] = 'hbnb_dev_db'
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = db_storage()
 else:
