746a015807d646b3347c940b7682a3612abac7f6
DINAMOW <meemoo102039@gmail.com>
2023-10-05 17:36:52 +0300

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,24 +4,22 @@
 sudo apt-get update
 sudo apt-get install nginx -y
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test
-sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p data/web_static/releases/
+sudo mkdir -p data/web_static/shared/
+sudo mkdir -p data/web_static/releases/test/
+sudo touch data/web_static/releases/test/index.html
 echo "<html>
 	<head>
 	</head>
 	<body>
 		<p>Hello World!</p>
 	</body>
-	</html>" | sudo tee  /data/web_static/releases/test/index.html
+</html>" | sudo tee  data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf data/web_static/releases/test data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "/listen 80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80 default_server;/ a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
