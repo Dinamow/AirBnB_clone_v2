020896fb25506daa35797fed9611be459eb77898
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-05 07:05:32 +0000

    Add  2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive"""
-from fabric.api import local, put, run
+from fabric.api import local, run, put, env
 from datetime import datetime
 from os import path
 
-env.hosts = [100.27.2.229, 54.160.117.223]
+env.hosts = ["100.27.2.229", "54.160.117.223"]
 env.user = "ubuntu"
+
 
 def do_pack():
     """All files in the folder web_static"""
@@ -23,15 +24,13 @@ def do_deploy(archive_path):
     """ distributes an archive to your web servers"""
     if not path.exists(archive_path):
         return None
-    put(archive_path, "/tmp/)
+    put(archive_path, "/tmp/")
     file_name = path.basename(archive_path)
-    target_dir_name = f"/data/web_static/releases/{archive_filename[:-4]}"
+    target_dir_name = f"/data/web_static/releases/{file_name[:-4]}"
     run(f"sudo mkdir -p /data/web_static/releases/{target_dir_name}")
     res = f"/data/web_static/releases/{target_dir_name}"
     run(f"sudo tar -xzf {archive_path} -C {res}")
     run(f"sudo rm /tmp/{file_name}")
-    run(f"sudo rm -rf /data/web_static/current")
+    run("sudo rm -rf /data/web_static/current")
     run(f"ln -s {res} /data/web_static/current")
-    except Exception as e:
-        return False
     return True
