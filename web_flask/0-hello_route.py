792ed2d896ba7e0660557fe0cfaba17f989f6ef3
abdelrhman mohammed <meemoo102039@gmail.com>
2023-10-20 16:29:11 +0000

    0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+AirBnB = Flask(__name__)
+
+@AirBnB.route("/", strict_slashes=False)
+def homepage():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    AirBnB.run(host="0.0.0.0" ,port=5000)
