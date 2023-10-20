cda506252c4b411bbd064ae73ab96ff605923fef
abdelrhman mohammed <meemoo102039@gmail.com>
2023-10-20 16:35:30 +0000

    task 0 pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,12 +2,15 @@
 """this is the flask hello"""
 from flask import Flask
 
+
 AirBnB = Flask(__name__)
+
 
 @AirBnB.route("/", strict_slashes=False)
 def homepage():
     """home page function"""
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    AirBnB.run(host="0.0.0.0" ,port=5000)
+    AirBnB.run(host="0.0.0.0", port=5000)
