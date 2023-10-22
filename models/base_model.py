2769ecd619a2ac767fc10222a6ac5e707a51edd9
abdelrhman mohammed <meemoo102039@gmail.com>
2023-10-22 13:53:36 +0000

    without any help

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,8 @@ class BaseModel:
                 kwargs['created_at'] = datetime.strptime(
                     kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,24 @@
-""" this is flask with storage """
-from models.__init__ import storage
-from flask import Flask
-
+#!/usr/bin/python3
+"""starts a Flask web application:"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """print a number"""
+    data = storage.all(State)
+    return render_template('7-states_list.html', data=data)
+
+
+@app.teardown_appcontext
+def cleanDb(error=None):
+    """close the session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
